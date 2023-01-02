199407cae666a0bc1ba3cb6e5504ef24788126ac
Nacrosolutions <42511725+Nacrosolutions@users.noreply.github.com>
2023-01-02 11:41:59 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ package linked_list;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello From nabham");
+        System.out.println("Hello From nabham and prokshima");
     }
 }
